dc86251edfec173c6894606d5698394b037526ae
Ahmad Satriadi <ahmadsatriadimm93@gmail.com>
2025-09-05 12:00:53 +0800

    add dashnoard

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -4,9 +4,9 @@ import { lusitana } from '@/app/ui/fonts';
 export default function AcmeLogo() {
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+      className={`${lusitana.className} flex flex-row items-center leading-none text-red-600`}
     >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+      <GlobeAltIcon className="h-12 w-12 rotate-[15deg] text-red-600" />
       <p className="text-[44px]">Acme</p>
     </div>
   );
